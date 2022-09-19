285ab158de59f638c011b81b7357fac500bd35b7
dknight34 <david.knight@gatech.edu>
2022-09-18 21:02:30 -0400

    Update manualAnalysisScript.m

--- Traction Control Modeling/Tire Data/manualAnalysisScript.m
+++ Traction Control Modeling/Tire Data/manualAnalysisScript.m
@@ -1,4 +1,4 @@
-load("B1965run15.mat")
+load("B1654run53.mat")
 P = P*0.145038;
 
 figure(2)
@@ -25,7 +25,9 @@ disp('Select data limits')
 mask = (ET < x(1)) | (ET > x(2));
 ET_trim = ET(~mask);
 FX_trim = FX(~mask);
+NFX_trim = NFX(~mask);
 FY_trim = FY(~mask);
+NFY_trim = NFY(~mask);
 FZ_trim = -FZ(~mask);
 SA_trim = -SA(~mask);
 NFY_trim = NFY(~mask);
@@ -34,43 +36,36 @@ IA_trim = IA(~mask);
 TSTC_trim = TSTC(~mask);
 MZ_trim = MZ(~mask);
 MX_trim = MX(~mask);
+SL_trim = SL(~mask);
+SR_trim = SR(~mask);
 
 pause(0.5)
 
-% xdft = fft(FZ_trim);
-%    % sampling interval -- assuming equal sampling
-%    DT = ET_trim(2)-ET_trim(1);
-%    % sampling frequency
-%    Fs = 1/DT;
-%    DF = Fs/length(ET_trim);
-%    freq = 0:DF:Fs/2;
-%    xdft = xdft(1:length(xdft)/2+1);
-%    figure
-%    plot(freq,abs(xdft))
-%% Camber Analysis
-load("B1965run15.mat")
-P = P*0.145038;
 
-figure
-hold on
-IAvec = [0,2,4];
-FZvec = [-850];
-pVec = [10];
-Legend = {};
-for i = 1:length(IAvec)
-    iaHigh = IAvec(i) + 1;
-    iaLow = IAvec(i) - 1;
-    for j = 1:length(FZvec)
-        fzHigh = FZvec(j) + 50;
-        fzLow = FZvec(j) - 50;
-        for k = 1:length(pVec)
-            pHigh = pVec(k) + 1;
-            pLow = pVec(k) - 1;
-            FYfilt = FY((fzHigh > FZ & fzLow < FZ) & (iaHigh > IA & iaLow < IA) & pHigh > P & pLow < P);
-            SAfilt = SA((fzHigh > FZ & fzLow < FZ) & (iaHigh > IA & iaLow < IA) & pHigh > P & pLow < P);
-            plot(SAfilt,FYfilt)
-            Legend = [Legend,['FZ = ',num2str(FZvec(j)),' IA = ',num2str(IAvec(i)),' P = ',num2str(pVec(k))]];
-        end
-    end
-end
-legend(Legend)
+%% Camber Analysis for cornering data
+% load("B1965run15.mat")
+% P = P*0.145038;
+% 
+% figure
+% hold on
+% IAvec = [0,2,4];
+% FZvec = [-850];
+% pVec = [10];
+% Legend = {};
+% for i = 1:length(IAvec)
+%     iaHigh = IAvec(i) + 1;
+%     iaLow = IAvec(i) - 1;
+%     for j = 1:length(FZvec)
+%         fzHigh = FZvec(j) + 50;
+%         fzLow = FZvec(j) - 50;
+%         for k = 1:length(pVec)
+%             pHigh = pVec(k) + 1;
+%             pLow = pVec(k) - 1;
+%             FYfilt = FY((fzHigh > FZ & fzLow < FZ) & (iaHigh > IA & iaLow < IA) & pHigh > P & pLow < P);
+%             SAfilt = SA((fzHigh > FZ & fzLow < FZ) & (iaHigh > IA & iaLow < IA) & pHigh > P & pLow < P);
+%             plot(SAfilt,FYfilt)
+%             Legend = [Legend,['FZ = ',num2str(FZvec(j)),' IA = ',num2str(IAvec(i)),' P = ',num2str(pVec(k))]];
+%         end
+%     end
+% end
+% legend(Legend)
